8b4bbbeb06e6089def4cc29e5df3981819293de2
Mustafa Mustafa <mamustafa@gtcc.edu>
2020-11-17 23:23:08 -0500

    database change

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: "root",
-    password: "mustafa",
-    database: "project_db",
-    host: "localhost",
+    username: "db6ssv114mja2dq0",
+    password: "u48mbskwnj6vbuiu",
+    database: "znjg49dtttcdw87e",
+    host: "d6q8diwwdmy5c9k9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     dialect: "mysql",
   },
   test: {
